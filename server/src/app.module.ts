ed2e639e54eba4bdb34f26bdf31b26859e25858a
ThamerAyachi <thamerjak3@gmail.com>
2022-11-20 15:35:30 +0100

    add config and env.example

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users/controllers/users.controller';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/services/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    UsersModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
