d43a22e492c2fe914b12fb8e9895336364b3305b
ThamerAyachi <thamerjak3@gmail.com>
2022-11-22 16:53:41 +0100

    add patient create dto

--- server/src/patient/dto/patientCreate.dto.ts
+++ server/src/patient/dto/patientCreate.dto.ts
@@ -1,8 +1,15 @@
-import { EmergencyContactCreateDto } from 'src/shared/dto/emergencyContactCreate.dto';
-import { ProfileCreateDto } from 'src/shared/dto/profileCreate.dto';
+import { Type } from 'class-transformer';
+import { EmergencyContactCreateDto } from 'src/shared/dto/EmergencyContactCreate.dto';
+import { MedicalInformationCreateDto } from 'src/shared/dto/MedicalInformationCreate.dto';
+import { ProfileCreateDto } from 'src/shared/dto/ProfileCreate.dto';
 
 export class PatientCreateDto {
+  @Type(() => ProfileCreateDto)
   profile: ProfileCreateDto;
 
+  @Type(() => EmergencyContactCreateDto)
   emergencyContact: EmergencyContactCreateDto;
+
+  @Type(() => MedicalInformationCreateDto)
+  medicalInformation: MedicalInformationCreateDto;
 }
