c4f525040785625e0c40266fc636e5812cdbb216
ThamerAyachi <thamerjak3@gmail.com>
2022-11-22 17:38:34 +0100

    add repos

--- server/src/patient/patient.module.ts
+++ server/src/patient/patient.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmergencyContactEntity } from 'src/typeorm/EmergencyContactEntity';
+import { MedicalInformationEntity } from 'src/typeorm/MedicalInformationEntity';
+import { PatientEntity } from 'src/typeorm/PatientEntity';
+import { ProfileEntity } from 'src/typeorm/ProfileEntity';
 import { PatientController } from './controllers/patient.controller';
 import { PatientService } from './services/patient.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      PatientEntity,
+      ProfileEntity,
+      MedicalInformationEntity,
+      EmergencyContactEntity,
+    ]),
+  ],
   controllers: [PatientController],
   providers: [PatientService],
 })
--- server/src/patient/services/patient.service.ts
+++ server/src/patient/services/patient.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EmergencyContactEntity } from 'src/typeorm/EmergencyContactEntity';
+import { MedicalInformationEntity } from 'src/typeorm/MedicalInformationEntity';
+import { PatientEntity } from 'src/typeorm/PatientEntity';
+import { ProfileEntity } from 'src/typeorm/ProfileEntity';
+import { Repository } from 'typeorm';
 import { PatientCreateDto } from '../dto/PatientCreate.dto';
 
 @Injectable()
 export class PatientService {
+  constructor(
+    @InjectRepository(PatientEntity)
+    private readonly PatientRepo: Repository<PatientEntity>,
+    @InjectRepository(ProfileEntity)
+    private readonly ProfileRepo: Repository<ProfileEntity>,
+    @InjectRepository(EmergencyContactEntity)
+    private readonly EmergencyContactRepo: Repository<EmergencyContactEntity>,
+    @InjectRepository(MedicalInformationEntity)
+    private readonly MedicalInformationRepo: Repository<MedicalInformationEntity>,
+  ) {}
+
   async createPatient(patientCreateDto: PatientCreateDto) {
     return { message: 'Done', patient: patientCreateDto };
   }
