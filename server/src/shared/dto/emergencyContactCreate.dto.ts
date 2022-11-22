3a0e5bf6c5b51cab5c6aaa1fd8908671fa11bde8
ThamerAyachi <thamerjak3@gmail.com>
2022-11-22 16:34:06 +0100

    add emergency contact create dto

---
+++ server/src/shared/dto/emergencyContactCreate.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsNotEmpty,
+  IsNumberString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class emergencyContactCreateDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  title: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  familyName: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  givenName: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  phoneNumber: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  mobileNumber: number;
+}
